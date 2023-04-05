cc3a7fd6d349d4524fc75ecb1027efc5be8d25c9
rikesh <rikesh@iitk.ac.in>
2023-04-05 22:48:24 +0530

    add help and Milestone3.pdf

--- Milestone_3/tests/test_25.java
+++ Milestone_3/tests/test_25.java
@@ -2,25 +2,24 @@ class short_circuit{
     // int printInt(int n, int y){
     //     return n+y;
     // }
-    int x;
-    void printInt();
+    int printInt(int n, int y){
+        return 1;
+    }
     public static int main(int k)
     {
-        this.x = 0;
-        this.printInt();
-        // for(int i = 0; i<i; i++, i++){
-        //     int abc = 1+2+3;
-        // }
+        for(int i = 0; i<i; i++, i++){
+            int abc = 1+2+3;
+        }
 
-        // int a = 0 + 2;
-        // float b = 2.3f;
-        // int arr[][][] = new int [10][5][4];
-        // arr[6][4+7*8][1] = 2+9*7;
+        int a = 0 + 2;
+        float b = 2.3f;
+        int arr[][][] = new int [10][5][4];
+        arr[6][4+7*8][1] = 2+9*7;
 
-        // int arr2[] = new int [6];
-        // arr2[1] = 1;
-        // int i = arr2[2]+2;
-        // printInt(a,3);
+        int arr2[] = new int [6];
+        arr2[1] = 1;
+        int i = arr2[2]+2;
+        printInt(a,3);
         return 46;
     }
  }
