142de5875db29eafe266a771465853bb21ca75ff
rikesh <rikesh@iitk.ac.in>
2023-04-20 16:42:02 +0530

    add assembly for binary operators

--- Assembly/main.s
+++ Assembly/main.s
@@ -1,62 +1,70 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 13, 0	sdk_version 13, 3
-	.globl	_caller                         ; -- Begin function caller
-	.p2align	2
-_caller:                                ; @caller
-; %bb.0:
-	sub	sp, sp, #32
-	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
-	add	x29, sp, #16
-	stur	w0, [x29, #-4]
-	str	w1, [sp, #8]
-	ldur	w8, [x29, #-4]
-	ldr	w9, [sp, #8]
-	add	w0, w8, w9
-	bl	_callee
-	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
-	add	sp, sp, #32
+	.file	"main.c"
+	.text
+	.globl	caller
+	.type	caller, @function
+caller:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movl	%esi, -8(%rbp)
+	movl	-4(%rbp), %edx
+	movl	-8(%rbp), %eax
+	addl	%edx, %eax
+	movl	%eax, %edi
+	call	callee
+	leave
 	ret
-                                        ; -- End function
-	.globl	_callee                         ; -- Begin function callee
-	.p2align	2
-_callee:                                ; @callee
-; %bb.0:
-	sub	sp, sp, #16
-	str	w0, [sp, #12]
-	ldr	w8, [sp, #12]
-	add	w0, w8, #1
-	add	sp, sp, #16
+	.size	caller, .-caller
+	.globl	callee
+	.type	callee, @function
+callee:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	movl	%edi, -4(%rbp)
+	movl	-4(%rbp), %eax
+	addl	$1, %eax
+	popq	%rbp
 	ret
-                                        ; -- End function
-	.globl	_main                           ; -- Begin function main
-	.p2align	2
-_main:                                  ; @main
-; %bb.0:
-	sub	sp, sp, #32
-	stp	x29, x30, [sp, #16]             ; 16-byte Folded Spill
-	add	x29, sp, #16
-	stur	wzr, [x29, #-4]
-	mov	w8, #1
-	str	w8, [sp, #8]
-	ldr	w8, [sp, #8]
-	subs	w8, w8, #1
-	cset	w8, ne
-	tbnz	w8, #0, LBB2_2
-	b	LBB2_1
-LBB2_1:
-	ldr	w0, [sp, #8]
-	mov	w1, #1
-	bl	_caller
-	b	LBB2_3
-LBB2_2:
-	ldr	w0, [sp, #8]
-	mov	w1, #2
-	bl	_caller
-	b	LBB2_3
-LBB2_3:
-	mov	w0, #0
-	ldp	x29, x30, [sp, #16]             ; 16-byte Folded Reload
-	add	sp, sp, #32
+	.size	callee, .-callee
+	.globl	main
+	.type	main, @function
+main:
+	pushq	%rbp
+	movq	%rsp, %rbp
+	subq	$16, %rsp
+	movl	$1, -4(%rbp)
+	cmpl	$1, -4(%rbp)
+	jne	.L6
+	movl	-4(%rbp), %eax
+	movl	$1, %esi
+	movl	%eax, %edi
+	call	caller
+	jmp	.L7
+.L6:
+	cmpl	$6, -4(%rbp)
+	jne	.L8
+	movl	$1, -4(%rbp)
+	jmp	.L7
+.L8:
+	cmpl	$7, -4(%rbp)
+	jne	.L9
+	movl	$2, -4(%rbp)
+	jmp	.L7
+.L9:
+	cmpl	$1000000, -4(%rbp)
+	jne	.L10
+	movl	$123, -4(%rbp)
+	jmp	.L7
+.L10:
+	movl	-4(%rbp), %eax
+	movl	$2, %esi
+	movl	%eax, %edi
+	call	caller
+.L7:
+	movl	$0, %eax
+	leave
 	ret
-                                        ; -- End function
-.subsections_via_symbols
+	.size	main, .-main
+	.ident	"GCC: (GNU) 12.2.1 20221121 (Red Hat 12.2.1-4)"
+	.section	.note.GNU-stack,"",@progbits
