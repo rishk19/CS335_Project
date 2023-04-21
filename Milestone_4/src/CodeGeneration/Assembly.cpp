24c35ca4988b6ce32c95c88d540325735e46b892
Rishabh Kothary <rishabhkothary76@gmail.com>
2023-04-21 13:49:46 +0530

    Add support for printing Assembly code

--- Milestone_4/src/CodeGeneration/Assembly.cpp
+++ Milestone_4/src/CodeGeneration/Assembly.cpp
@@ -3,171 +3,6 @@
 using namespace std;
 #define ASSEMBLER_ERROR -1
 
-string getOperator(Op op){
-    switch (op){
-        case NotOperator_:
-            return "notq";
-            break;
-        case Tilde_:
-            return "notq";
-            break;
-        case Empty_:
-            return "";
-            break;
-        case EqualToEqualTo_:
-            return "cmp";
-            break;
-        case GreaterThanEqualTo_:
-            return "setge";
-            break;
-        case LessThanEqualTo_:
-            return "<=";
-            break;
-        case NotEqualTo_:
-            return "!=";
-            break;
-        case AndOperator_:
-            return "&&";
-            break;
-        case OrOperator_:
-            return "||";
-            break;
-        case PlusPlus_:
-            return "++";
-            break;
-        case MinusMinus_:
-            return "--";
-            break;
-        case Addition_:
-            return "addq";
-            break;
-        case Substraction_:
-            return "-";
-            break;
-        case Product_:
-            return "*";
-            break;
-        case Divide_:
-            return "/";
-            break;
-        case BitwiseAnd_:
-            return "&";
-            break;
-        case BitwiseOr_:
-            return "|";
-            break;
-        case CircumFlex_:
-            return "^";
-            break;
-        case Modulo_:
-            return "%";
-            break;
-        case LeftShit_:
-            return "<<";
-            break;
-        case RightShift_:
-            return ">>";
-            break;
-        case TripleGreaterThan_:
-            return ">>>";
-            break;
-        case AdditionEqualTo_:
-            return "+=";
-            break;
-        case SubstractionEqualTo_:
-            return "-=";
-            break;
-        case ProductEqualTo_:
-            return "*=";
-            break;
-        case DivideEqualTo_:
-            return "/=";
-            break;
-        case BitWiseAndEqualTo_:
-            return "&=";
-            break;
-        case BitWiseOrEqualTo_:
-            return "|=";
-            break;
-        case CircumFlexEqualTo_:
-            return "^=";
-            break;
-        case ModuloEqualTo_:
-            return "%=";
-            break;
-        case LeftShitEqualTo_:
-            return "<<=";
-            break;
-        case RightShiftEqualTo_:
-            return ">>=";
-            break;
-        case TripleGreaterThanEqualTo_:
-            return ">>>=";
-            break;
-        case GreaterThan_:
-            return ">";
-            break;
-        case LessThan_:
-            return "<";
-            break;
-        case Compare_and_Jne_:
-            return "cmp";
-            break;
-        case Compare_and_Je_:
-            return "cmp";
-            break;
-        case Jne_:
-            return "Jne";
-            break;
-        case Jmp_:
-            return "Jmp";
-            break;
-        case Label_:
-            return "Label";
-            break;
-        case ArrayAccess_:
-            return "ArrayAccess";
-            break;
-        case LoadArray_:
-            return "LoadArray";
-            break;
-        case Pushq_:
-            return "Pushq";
-            break;
-        case Movq_:
-            return "Movq";
-            break;
-        case Popq_:
-            return "Popq";
-            break;
-        case Push_:
-            return "Push";
-            break;
-        case Mov_:
-            return "Mov";
-            break;
-        case Pop_:
-            return "Pop";
-            break;
-        case Retq_:
-            return "retq";
-            break;
-        case Addq_:
-            return "addq";
-            break;
-        case Subq_:
-            return "subq";
-            break;
-        case Callq_:
-            return "callq";
-            break;
-        default:
-            break;
-    }
-    return "no matching operator";
-}
-
-
 vector<string> quad_to_assembly(struct Quad* quad ){
     vector<string> assembly_template;
     string r12 = "%r12";
@@ -184,6 +19,7 @@ vector<string> quad_to_assembly(struct Quad* quad ){
         else{
             assembly_template.push_back(store_inst(quad->result, r12, quad->my_table));
         }
+        break;
         
     
     case Substraction_:
@@ -204,12 +40,14 @@ vector<string> quad_to_assembly(struct Quad* quad ){
         assembly_template.push_back(load_inst(quad->arg_2, r13, quad->my_table));
         assembly_template.push_back("imulq " + r12 + " " + r13);
         assembly_template.push_back(store_inst(quad->result, r13, quad->my_table));
+        break;
 
     case Divide_:
         assembly_template.push_back(load_inst(quad->arg_1, r12, quad->my_table));
         assembly_template.push_back(load_inst(quad->arg_2, r13, quad->my_table));
         assembly_template.push_back("subq " + r12 + " " + r13);
         assembly_template.push_back(store_inst(quad->result, r13, quad->my_table));
+        break;
 
 
     default:
@@ -245,4 +83,29 @@ string store_inst(struct Argument arg, string reg, struct SymbolTable * my_table
         assembly += to_string(symb->offset)+"(%rbp)";
     }
     return assembly;
+}
+
+void generateAssembly(struct GlobalSymbolTable * glob_table)
+{
+    if(glob_table == NULL){
+        cout << "Empty Global Table" << endl;
+    }
+    struct GlobalSymbol glob_entry;
+    for (int i = 0; i< glob_table->entries.size(); i++)
+    {   
+        cout << endl;
+        struct Quad quad;
+        vector<string>code;
+        glob_entry = glob_table->entries[i];
+        for (int j = 0; j< glob_entry.tac.quad.size(); j++)
+        {
+            cout << view_quad(&glob_entry.tac.quad[j]);
+            cout << endl;
+            //quad_to_assembly(&glob_entry.tac.quad[j]);
+            // for (int k =0; k<code.size() ; k++){
+            //     cout << code[j] << endl;
+            // }
+        }
+    }
+    return;
 }
--- Milestone_4/src/CodeGeneration/Assembly.hpp
+++ Milestone_4/src/CodeGeneration/Assembly.hpp
@@ -1,10 +1,16 @@
 #ifndef ASSEMBLY_H_
 #define ASSEMBLY_H_
 
+#include "../DataStructures/GlobalSymbolTable.hpp"
+#include "../DataStructures/SymbolTable.hpp"
+#include "../DataStructures/Tac.hpp"
+#include<bits/stdc++.h>
+
 
 vector<string> quad_to_assembly(struct Quad* quad );
 
 string load_inst(struct Argument arg, string reg, struct SymbolTable * my_table);
 string store_inst(struct Argument arg, string reg, struct SymbolTable * my_table);
+void generateAssembly(struct GlobalSymbolTable * glob_table);
 
 #endif
